887df826d6aeb125b632e13ac8d9b132c6319eca
yidafu <yidafu@youzan.com>
2019-11-18 22:28:28 +0800

    upgrade dependence

--- config.js
+++ config.js
@@ -1,28 +1,27 @@
 module.exports = {
-    dependence: {
-        npm: {
-
-            "babel-eslint": "7.2.1",
-            "eslint-config-airbnb": "14.1.0",
-            "eslint-plugin-import": "2.2.0",
-            "eslint-plugin-jsx-a11y": "4.0.0",
-            "eslint-plugin-lean-imports": "0.3.3",
-            "eslint-plugin-react": "6.10.3",
-            "stylelint": "7.12.0",
-            "stylelint-config-standard": "16.0.0",
-            "stylelint-processor-html": "1.0.0",
-            "@typescript-eslint/eslint-plugin": "^2.7.0",
-            "@typescript-eslint/parser": "^2.7.0",
-            "eslint": "^6.6.0",
-            "eslint-config-airbnb": "^18.0.1",
-            "husky": "^3.0.9",
-            "typescript": "^3.7.2"
-        }
-    },
-    plan: {
-        node: ['.eslintrc_node.json', 'commitlint.config.js'],
-        web: ['.eslintrc_web.json', '.stylelintrc.json'],
-        default: ['.eslintrc_node.json', '.stylelintrc.json']
-    },
-    initHooks: 'update_git_hooks.sh'
+  dependence: {
+    npm: {
+      "@typescript-eslint/eslint-plugin": "2.7.0",
+      "@typescript-eslint/parser": "2.7.0",
+      "eslint-config-airbnb": "18.0.1",
+      "eslint-plugin-lean-imports": "0.3.3",
+      "eslint-plugin-import": "2.18.2",
+      "husky": "3.0.9",
+      "stylelint": "7.12.0",
+      "stylelint-config-standard": "16.0.0",
+      "stylelint-processor-html": "1.0.0",
+      "typescript": "3.7.2"
+      "babel-eslint": "10.0.3",
+      "eslint-config-airbnb": "14.1.0",
+      "eslint-plugin-jsx-a11y": "6.2.3",
+      "eslint-plugin-react": "7.14.3",
+      "eslint": "6.6.0",
+    }
+  },
+  plan: {
+    node: ['.eslintrc_node.json', 'commitlint.config.js'],
+    web: ['.eslintrc_web.json', '.stylelintrc.json'],
+    default: ['.eslintrc_node.json', '.stylelintrc.json']
+  },
+  initHooks: 'update_git_hooks.sh'
 }
