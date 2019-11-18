ba8e567f8b537a802630a41573e72d2f94e0ea43
yidafu <yidafu@youzan.com>
2019-11-18 21:43:08 +0800

    update config files

--- config.js
+++ config.js
@@ -1,27 +1,28 @@
 module.exports = {
     dependence: {
         npm: {
-            "eslint": "3.19.0",
+
             "babel-eslint": "7.2.1",
             "eslint-config-airbnb": "14.1.0",
-            "eslint-config-vue": "2.0.2",
             "eslint-plugin-import": "2.2.0",
             "eslint-plugin-jsx-a11y": "4.0.0",
             "eslint-plugin-lean-imports": "0.3.3",
             "eslint-plugin-react": "6.10.3",
-            "eslint-plugin-vue": "2.0.1",
             "stylelint": "7.12.0",
             "stylelint-config-standard": "16.0.0",
-            "stylelint-processor-html": "1.0.0"
+            "stylelint-processor-html": "1.0.0",
+            "@typescript-eslint/eslint-plugin": "^2.7.0",
+            "@typescript-eslint/parser": "^2.7.0",
+            "eslint": "^6.6.0",
+            "eslint-config-airbnb": "^18.0.1",
+            "husky": "^3.0.9",
+            "typescript": "^3.7.2"
         }
     },
     plan: {
-        es6: ['.eslintrc_es6.json', '.stylelintrc.json'],
-        es5: ['.eslintrc_es5.json', '.stylelintrc.json'],
-        node: ['.eslintrc_node.json'],
-        vue: ['.eslintrc_vue.json', '.stylelintrc.json'],
-        react: ['.eslintrc_react.json', '.stylelintrc.json'],
-        default: ['.eslintrc_es6.json', '.stylelintrc.json']
+        node: ['.eslintrc_node.json', 'commitlint.config.js', 'commitlint.config.js'],
+        web: ['.eslintrc_web.json', '.stylelintrc.json', 'commitlint.config.js'],
+        default: ['.eslintrc_node.json', '.stylelintrc.json', 'commitlint.config.js']
     },
     initHooks: 'update_git_hooks.sh'
 }
