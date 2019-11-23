9c451a6aff04dfcea3088c629f66bb4e773f1e76
dov-yih <yidafu90@qq.com>
2019-11-23 21:02:22 +0800

    fix: ignore commitlint now

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ module.exports = {
     }
   },
   plan: {
-    node: ['.eslintrc_node.json', 'commitlint.config.js'],
+    node: ['.eslintrc_node.json', '.stylelintrc.json'],
     web: ['.eslintrc_web.json', '.stylelintrc.json'],
     default: ['.eslintrc_node.json', '.stylelintrc.json']
   },
